cd8f6ea904a1e8486691c49597f2bf6211edf482
d3vex <sebastian.dero@yahoo.de>
2015-11-13 14:58:26 +0100

    fixes

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -47,6 +47,34 @@ loadCV = function () {
 
 $("document").ready(function () {
 
+    function demoFromHTML() {
+        var pdf = new jsPDF('p', 'pt', 'letter');
+        
+        specialElementHandlers = {
+            '#bypassme': function (element, renderer) {
+                return true;
+            }
+        };
+        
+        margins = {
+            top: 80,
+            bottom: 60,
+            left: 40,
+            width: 522
+        };
+
+        pdf.fromHTML(
+            'documents/cv.html#content', // HTML string or DOM elem ref.
+            margins.left, // x coord
+            margins.top, { // y coord
+                'width': margins.width, // max width of content on PDF
+                'elementHandlers': specialElementHandlers
+            },
+
+            function (dispose) {
+                pdf.save('Test.pdf');
+            }, margins);
+    }
 
     if (window.location.hash == "#home") {
         loadHome();
@@ -57,7 +85,7 @@ $("document").ready(function () {
     } else {
         loadCV();
     }
-    
+
     $("#menu_home").on("click", function () {
         loadHome();
     });
