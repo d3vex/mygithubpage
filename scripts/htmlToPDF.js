8a509e3fda40d9bf0ad8f20e7140a6ede7cc7970
d3vex <sebastian.dero@yahoo.de>
2015-11-18 08:46:45 +0100

    html to pdf with css

--- scripts/htmlToPDF.js
+++ scripts/htmlToPDF.js
@@ -1,28 +1,50 @@
-function demoFromHTML(id, name) {
-        var pdf = new jsPDF('p', 'pt', 'letter');
-        
+form = $('#pdfContent');
+cache_width = form.width(),
+    a4 = [595.28, 841.89]; // for a4 size paper width and heights
+
+function pdfFromHTML() {
+        $('body').scrollTop(0);
+        /*var pdf = new jsPDF('p', 'pt', 'letter');
         specialElementHandlers = {
             '#bypassme': function (element, renderer) {
                 return true;
             }
         };
-        
-        margins = {
+
+        var margins = {
             top: 80,
             bottom: 60,
             left: 40,
             width: 522
         };
-    
+        var content = $('#pdfContent').get(0);
         pdf.fromHTML(
-            $('#' + id).html(),
+            content,
             margins.left,
             margins.top, {
                 'width': margins.width,
                 'elementHandlers': specialElementHandlers
-            },
+            });
 
-            function (dispose) {
-                pdf.save(name);
-            }, margins);
+        pdf.save("dataurlnewwindow");
+        */
+        getCanvas().then(function (canvas) {
+            var
+                img = canvas.toDataURL("image/png"),
+                doc = new jsPDF({
+                    unit: 'px',
+                    format: 'a4'
+                });
+            doc.addImage(img, 'JPEG', 20, 20);
+            doc.save('techumber-html-to-pdf.pdf');
+            form.width(cache_width);
+        });
     }
+    // create canvas object
+function getCanvas() {
+    form.width((a4[0] * 1.33333) - 80).css('max-width', 'none');
+    return html2canvas(form, {
+        imageTimeout: 2000,
+        removeContainer: true
+    });
+}
