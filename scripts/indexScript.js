80ab9986e6a4731e5aac741a512d277b23dfa76a
BuildTools <unconfigured@null.spigotmc.org>
2015-11-26 17:32:03 +0100

    beautify

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,67 +1,67 @@
-clearMenu = function () {
+clearMenu = function() {
     document.getElementById("menu_home").className = "main-menubar-item mdl-navigation__link mdl-typography--text-uppercase";
     document.getElementById("menu_portfolio").className = "main-menubar-item mdl-navigation__link mdl-typography--text-uppercase";
     document.getElementById("menu_cv").className = "main-menubar-item mdl-navigation__link mdl-typography--text-uppercase";
 };
-loadHome = function () {
+loadHome = function() {
     clearMenu();
     document.getElementById("menu_home").className = "main-menubar-item mdl-navigation__link mdl-typography--text-uppercase menu-item-active";
     document.title = "Sebastian De Ro - Home";
-    jQuery("#content").fadeOut(100, function () {
+    jQuery("#content").fadeOut(100, function() {
         document.getElementById("content").innerHTML = "";
         jQuery("#content").load("documents/home.html");
         jQuery("#content").fadeIn(100);
     });
 };
-loadPortfolio = function () {
+loadPortfolio = function() {
     clearMenu();
     document.getElementById("menu_portfolio").className = "main-menubar-item mdl-navigation__link mdl-typography--text-uppercase menu-item-active";
     document.title = "Sebastian De Ro - Portfolio";
-    jQuery("#content").fadeOut(100, function () {
+    jQuery("#content").fadeOut(100, function() {
         document.getElementById("content").innerHTML = "";
         jQuery("#content").load("documents/portfolio.html");
         jQuery("#content").fadeIn(200);
     });
 };
-loadCV = function () {
+loadCV = function() {
     clearMenu();
     document.getElementById("menu_cv").className = "main-menubar-item mdl-navigation__link mdl-typography--text-uppercase menu-item-active";
     document.title = "Sebastian De Ro - CV";
-    jQuery("#content").fadeOut(100, function () {
+    jQuery("#content").fadeOut(100, function() {
         document.getElementById("content").innerHTML = "";
         jQuery("#content").load("documents/cv.html");
         jQuery("#content").fadeIn(200);
     });
 };
 
-$("document").ready(function () {
+$("document").ready(function() {
 
     if (window.location.hash == "#home") {
         loadHome();
     } else if (window.location.hash == "#portfolio") {
         loadPortfolio();
-    } else if (window.location.hash == "#cv"){
+    } else if (window.location.hash == "#cv") {
         loadCV();
-    } else if (window.location.hash == ""){
+    } else if (window.location.hash == "") {
         loadHome();
     }
-    $("#menu_home").on("click", function () {
+    $("#menu_home").on("click", function() {
         loadHome();
     });
-    $("#menu_portfolio").on("click", function () {
+    $("#menu_portfolio").on("click", function() {
         loadPortfolio();
     });
-    $("#menu_cv").on("click", function () {
+    $("#menu_cv").on("click", function() {
         loadCV();
     });
-    
-    $("#mobile_menu_home").on("click", function () {
+
+    $("#mobile_menu_home").on("click", function() {
         loadHome();
     });
-    $("#mobile_menu_portfolio").on("click", function () {
+    $("#mobile_menu_portfolio").on("click", function() {
         loadPortfolio();
     });
-    $("#mobile_menu_cv").on("click", function () {
+    $("#mobile_menu_cv").on("click", function() {
         loadCV();
     });
 });
