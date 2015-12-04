e708238b986f2245ca1cbf21ac246288c1faad23
BuildTools <unconfigured@null.spigotmc.org>
2015-12-04 15:01:56 +0100

    regionalization and update and correction and stuff

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,3 +1,6 @@
+var lang;
+var currentSite;
+
 clearMenu = function() {
   document.getElementById("menu_home").className = "main-menubar-item mdl-navigation__link mdl-typography--text-uppercase";
   document.getElementById("menu_portfolio").className = "main-menubar-item mdl-navigation__link mdl-typography--text-uppercase";
@@ -5,6 +8,8 @@ clearMenu = function() {
 };
 
 loadPage = function(menuitem, title, docPath) {
+  currentDocPath=docPath;
+  currentMenuitem=menuitem;
   if ($("#page-content").scrollTop() == 0) {
     if (menuitem != "none") {
       clearMenu();
@@ -38,30 +43,70 @@ loadPage = function(menuitem, title, docPath) {
 };
 
 loadHome = function() {
-  loadPage("menu_home", "Sebastian De Ro - Home", "documents/home.html");
+  currentSite="home";
+  if (lang == "de")
+    loadPage("menu_home", "Sebastian De Ro - Home", "documents/home_de.html");
+  else
+    loadPage("menu_home", "Sebastian De Ro - Home", "documents/home.html");
 };
 loadPortfolio = function() {
-  loadPage("menu_portfolio", "Sebastian De Ro - Portfolio", "documents/portfolio.html");
+  currentSite="portfolio";
+  if (lang == "de")
+    loadPage("menu_portfolio", "Sebastian De Ro - Portfolio", "documents/portfolio_de.html");
+  else
+    loadPage("menu_portfolio", "Sebastian De Ro - Portfolio", "documents/portfolio.html");
 };
 loadCV = function() {
-  loadPage("menu_cv", "Sebastian De Ro - CV", "documents/cv.html");
+  currentSite="cv";
+  if (lang == "de")
+    loadPage("menu_cv", "Sebastian De Ro - CV", "documents/cv_de.html");
+  else
+    loadPage("menu_cv", "Sebastian De Ro - CV", "documents/cv.html");
 };
 loadSitenotice = function() {
-  loadPage("none", "Sebastian De Ro - Site notice", "documents/impressum.html");
+  currentSite="sitenotice";
+  if (lang == "de")
+    loadPage("none", "Sebastian De Ro - Site notice", "documents/impressum_de.html");
+  else
+    loadPage("none", "Sebastian De Ro - Impressum", "documents/impressum.html");
+};
+
+switchEn = function() {
+  lang = "en";
+  $(".de").hide();
+  $(".en").show();
+  if(currentSite=="portfolio") loadPortfolio();
+  else if(currentSite=="cv") loadCV();
+  else if(currentSite=="sitenotice") loadSitenotice();
+  else loadHome();
+};
+
+switchDe = function() {
+  lang = "de";
+  $(".en").hide();
+  $(".de").show();
+  if(currentSite=="portfolio") loadPortfolio();
+  else if(currentSite=="cv") loadCV();
+  else if(currentSite=="sitenotice") loadSitenotice();
+  else loadHome();
 };
 
 $("document").ready(function() {
+  $(".de").hide();
+  lang = navigator.language;
+
+  if(lang == "de") switchDe();
 
   $(".cover-sheet").hide();
 
   if (window.location.hash == "#home") {
-    loadHome();
+    currentSite=loadHome();
   } else if (window.location.hash == "#portfolio") {
-    loadPortfolio();
+    currentSite=loadPortfolio();
   } else if (window.location.hash == "#cv") {
-    loadCV();
+    currentSite=loadCV();
   } else if (window.location.hash == "") {
-    loadHome();
+    currentSite=loadHome();
   }
   $("#menu_home").on("click", function() {
     loadHome();
@@ -85,6 +130,12 @@ $("document").ready(function() {
 
   $("#linkedin-button").on("click", function() {
     window.open("http://www.linkedin.com/in/sebastiandero", '_blank').focus();
+  });
+  $("#langEn").on("click", function() {
+    switchEn();
+  });
+  $("#langDe").on("click", function() {
+    switchDe();
   });
 
   $(".mdl-layout-title").on("click", function() {
